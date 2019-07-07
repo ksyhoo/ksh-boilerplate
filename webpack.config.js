508d64e43058e85548a18e61288647668aeff24b
Krzysztof Kulikowski <krzysztof.kulikowski@eyedea.io>
2019-07-07 10:58:03 +0200

    chore(): updated condigs

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,52 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const {TsConfigPathsPlugin} = require('awesome-typescript-loader');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/index.tsx',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
-    },
-    devServer: {
-        hot: true,
-        liveReload: true,
-        quiet: true,
-        compress: true,
-
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: 
-                    [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                plugins: [
-                                    "@babel/plugin-syntax-dynamic-import",
-                                    "@babel/plugin-proposal-export-default-from",
-                                ]
-                            }
-                        },
-                        {
-                            loader: 'eslint-loader',
-                        },
-                        {
-                            loader: 'awesome-typescript-loader',
-                        }
-                    ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-            logLevel: 'warn',
-        }),
-        new FriendlyErrorsWebpackPlugin({
-            compilationSuccessInfo: {
-                messages: [
-                    'Server: http://localhost:8080',
-                    'Bundle: http://localhost:8888',
-                ],
-            },
-        }),
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [new TsConfigPathsPlugin()],
+  },
+  devServer: {
+    hot: true,
+    liveReload: true,
+    quiet: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
     ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      logLevel: 'warn',
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['Server: http://localhost:8080', 'Bundle: http://localhost:8888'],
+      },
+    }),
+  ],
 };
